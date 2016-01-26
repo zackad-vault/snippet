1bcf8917f19b7435a5b040f177d7f4184ed9d76e
zackad <cahsiwarak@gmail.com>
2016-01-26 13:19:00 +0700

    use having instaed of where clause

--- sql/get_distance.sql
+++ sql/get_distance.sql
@@ -9,13 +9,7 @@ round(get_distance(substring_index(substring_index(koordinat, ',', 1), ',', -1),
 	@orig_lat, 
 	@orig_lng), 2) as distance
 from ci_lokasi
-where get_distance(
-	substring_index(substring_index(koordinat, ',', 1), ',', -1), 
-	trim(substr(koordinat, locate(',', koordinat)+1)), 
-	@orig_lat, 
-	@orig_lng) < 7
-#and distance < 9
-#where trim(substr(koordinat, locate(',', koordinat)+1)) > 108
+having distance < 3
 order by distance
 limit 3
 ;
