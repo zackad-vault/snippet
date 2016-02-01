aa9d13e2cbf418d54ea144d2ea1a1cc40dac96b3
zackad <cahsiwarak@gmail.com>
2016-02-02 03:56:31 +0700

    use comparation instead of populate to table

--- sql/spatial_distance.sql
+++ sql/spatial_distance.sql
@@ -11,23 +11,25 @@ BEGIN
 	DECLARE dist DOUBLE;
 	DECLARE i INT;
 	DECLARE jum_titik INT;
+	DECLARE temp DOUBLE;
 	
 	SET jum_titik = numpoints(pline);
 	SET i = 1;
-	
+	SET dist = 0;
 	while i < jum_titik do
-		insert into ci_distance_temp values (
-			get_distance(
+		set temp = get_distance(
 				y(p1), 
 				x(p1), 
 				y(pointn(pline, i)), 
 				x(pointn(pline, i))
-			)
-		);
+			);
+		if(dist < temp or dist = 0) then
+			set dist = temp;
+		end if;
 		set i = i + 1;
 	end while;
 	
-	set dist = (select min(distance) from ci_distance_temp);
+	#set dist = (select min(distance) from ci_distance_temp);
 	RETURN dist;
 END $$
 DELIMITER ;
